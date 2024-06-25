8070d8cdd56943a45fc50d5da419e86b3157f7d8
irisrocio1979 <42756418+irisrocio1979@users.noreply.github.com>
2024-06-25 10:53:11 -0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,63 @@
+# https://huggingface.co/datasets/somosnlp-hackathon-2022/spanish-to-quechua
+
+from flask import Flask, request, jsonify, render_template
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+app = Flask(__name__)
+
+model_name = 'hackathon-pln-es/t5-small-finetuned-spanish-to-quechua'
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    text = request.json['text']
+    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True)
+    translated = model.generate(**inputs)
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return jsonify({'translated_text': translated_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import uvicorn
+
+app = FastAPI()
+
+# Montar archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Configurar templates
+templates = Jinja2Templates(directory="templates")
+
+# Cargar el modelo y el tokenizador
+model_name = 'hackathon-pln-es/t5-small-finetuned-spanish-to-quechua'
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/translate")
+async def translate(request: Request):
+    data = await request.json()
+    text = data['text']
+    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True)
+    translated = model.generate(**inputs)
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return {"translated_text": translated_text}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+"""
